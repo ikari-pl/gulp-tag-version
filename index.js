ed493d4031f99e5680352d19664a34628a6f407d
Marcello de Sales <Marcello_DeSales@intuit.com>
2014-11-16 01:09:54 -0800

    Issue #6: Git reset the bump commit when the tag already exists.
    This is performed by using the git module "gift" and not issuing
    a git tag if it exists.
    
    https://github.com/notatestuser/gift#reporesettreeish-options-callback
    
    After this point, we just reset HEAD^.
    
    *	modified:   index.js
    - Implementing the steps to get the git repo, and the list of tags
    
    *	modified:   package.json
    - Bumping the version manually to 1.1.1.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var map = require('map-stream'),
   gutil = require('gulp-util'),
-  git = require('gulp-git')
+  git = require('gulp-git'),
+  gift = require('gift');
 
 module.exports = function(opts) {
   if(!opts) opts = {}
@@ -16,7 +17,33 @@ module.exports = function(opts) {
     var json = JSON.parse(file.contents.toString()),
     	tag = opts.prefix+json[opts.key]
     gutil.log('Tagging as: '+gutil.colors.cyan(tag))
-    git.tag(tag, 'tagging as '+tag, opts)
+
+    // gift is a full-fledge git plugin
+    var APP_DIR = process.env.PWD;
+    var repo = gift(APP_DIR);
+
+    // Retrieve all the existing tags
+    repo.tags(function(err, tags) {
+      // Collect their names
+      var tagNames = tags.map(function(tag) {
+         return tag.name;
+      });
+
+      // If it does not exist, we can tag safely.
+      if (tagNames.indexOf(tag) < 0) {
+        git.tag(tag, 'tagging as '+tag, opts);
+
+      } else {
+        // Revert the "bump" commit because the command would fail.
+        gutil.log("Tag " + gutil.colors.cyan(tag) + " exists! Revering commit...");
+        repo.reset("HEAD^", function(err) {
+          if (err) {
+            return cb(new Error(err));
+          }
+        });
+      }
+    });
+
     cb(null, file)
   }
 
