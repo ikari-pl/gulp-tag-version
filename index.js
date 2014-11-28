eb0f8258e8a1bf4030d02712b60da6ffab61bfbc
Cezar “ikari” Pokorski <git@ikari.pl>
2014-11-28 09:20:22 +0100

    reformat

--- index.js
+++ index.js
@@ -10,36 +10,36 @@ var map = require('map-stream'),
  * @param opts.version {string?} Alternatively, just pass the version string here. Default: undefined.
  */
 module.exports = function(opts) {
-  if(!opts) opts = {}
-  if(!opts.key) opts.key = 'version'
-  if(typeof opts.prefix === 'undefined') opts.prefix = 'v'
-  if(typeof opts.push === 'undefined') opts.push = true
+  if (!opts) opts = {};
+  if (!opts.key) opts.key = 'version';
+  if (typeof opts.prefix === 'undefined') opts.prefix = 'v';
+  if (typeof opts.push === 'undefined') opts.push = true;
 
   function tagItInGit(file, callback) {
     var version = opts.version, // OK if undefined at this time
       repo = gift(opts.cwd || process.env.PWD),
       tag;
 
-    if(!opts.version) {
-      if(file.isNull()) return callback(null, file)
-      if(file.isStream()) return callback(new Error('gulp-tag-version: streams not supported'))
+    if (!opts.version) {
+      if (file.isNull()) return callback(null, file);
+      if (file.isStream()) return callback(new Error('gulp-tag-version: streams not supported'));
 
       var json = JSON.parse(file.contents.toString());
-      version = json[opts.key]
+      version = json[opts.key];
     }
-    tag = opts.prefix+version
-    gutil.log('Tagging as: '+gutil.colors.cyan(tag))
+    tag = opts.prefix + version;
+    gutil.log('Tagging as: ' + gutil.colors.cyan(tag));
 
     // Retrieve all the existing tags
     repo.tags(function(err, tags) {
       // Collect their names
       var tagNames = tags.map(function(tag) {
-         return tag.name;
+        return tag.name;
       });
 
       // If it does not exist, we can tag safely.
       if (tagNames.indexOf(tag) < 0) {
-        repo.create_tag(tag, opts, function (err) {
+        repo.create_tag(tag, opts, function(err) {
           callback(err, file);
         });
       } else {
@@ -50,6 +50,5 @@ module.exports = function(opts) {
     });
   }
 
-
-  return map(tagItInGit)
+  return map(tagItInGit);
 };
